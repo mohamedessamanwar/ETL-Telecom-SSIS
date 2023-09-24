029545aa0698ad98f9f4c6e4e4204ee0332ab700
mohamedessamanwar <103913494+mohamedessamanwar@users.noreply.github.com>
2023-09-24 07:42:29 +0300

    Add files via upload

---
+++ Files/sql_files/SQLQuery1.sql
@@ -0,0 +1,21 @@
+truncate table fact_transaction 
+select * from fact_transaction 
+truncate table  error2
+truncate table  error1
+truncate table  dim_audit
+select * from dim_audit ;
+select * from error1 ;
+select * from error2;
+
+-- select max(batch_id)+1 as batch_id from dim_audit
+INSERT INTO dbo.dim_audit (
+    batch_id,
+    package_name, 
+    file_name, 
+    rows_extracted, 
+    rows_inserted, 
+    rows_rejected, 
+    SuccessfulProcessingInd
+)
+OUTPUT inserted.id as audit_id
+VALUES (?, ?, ?, NULL, NULL, NULL, 'N')
